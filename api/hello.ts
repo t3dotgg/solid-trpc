21822b913e843058ea3b1456086de7814d05b114
theo <me@t3.gg>
2021-12-18 00:15:37 -0800

    Kill unused hello endpoint

--- api/hello.ts
+++
@@ -1,9 +0,0 @@
-import type { VercelRequest, VercelResponse } from "@vercel/node";
-
-export default function handler(
-  request: VercelRequest,
-  response: VercelResponse
-) {
-  const { name } = request.query;
-  response.status(200).send(`Hello ${name}!`);
-}
