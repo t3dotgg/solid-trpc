d42bfa23433f9d2498baf8c6ba0a26232c055b35
theo <me@t3.gg>
2021-12-18 17:58:11 -0800

    style up

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { Component } from "solid-js";
 
-import logo from "./logo.svg";
 import styles from "./App.module.css";
 import { createTrpcQuery } from "./lib/trpc";
 
@@ -10,12 +9,14 @@ const App: Component = () => {
   return (
     <div class={styles.App}>
       <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
         <h2>{data()?.greeting ?? "Loading..."}</h2>
         <p>
           The content above is loaded from a tRPC endpoint hosted via a Vercel
           serverless function
         </p>
+        <a href="https://github.com/TheoBr/solid-trpc" class={styles.link}>
+          Github Repo
+        </a>
       </header>
     </div>
   );
